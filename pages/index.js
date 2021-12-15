5b18c38b22920ba41aced7dd4080b9fb3a299c1a
glowdex <glowdexco@gmail.com>
2021-12-15 14:42:46 -0600

    twitter image

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,12 @@ export default function Home() {
       <meta
       property="og:image"
       content="https://blog.glowdexapp.com/wp-content/uploads/2021/12/sitepreview.png"/>
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:site" content="@glowdexapp">
-      <meta name="twitter:creator" content="@glowdexapp">
-      <meta name="twitter:title" content="The Get List.">
-      <meta name="twitter:description" content="a simple, no-fuss wishlist for beauty lovers. get a url to share on your profile for the holidays! 🎁">
-      <meta name="twitter:image" content="https://blog.glowdexapp.com/wp-content/uploads/2021/12/sitepreview.png"></meta>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@glowdexapp" />
+      <meta name="twitter:creator" content="@glowdexapp" />
+      <meta name="twitter:title" content="The Get List." />
+      <meta name="twitter:description" content="a simple, no-fuss wishlist for beauty lovers. get a url to share on your profile for the holidays! 🎁" />
+      <meta name="twitter:image" content="https://blog.glowdexapp.com/wp-content/uploads/2021/12/sitepreview.png" />
     </Head>
     <div className="md:grid md:grid-cols-2 pt-12">
       <div className="md:col-span-1 px-8 sm:px-20 pt-36 pb-12 sm:py-36">
